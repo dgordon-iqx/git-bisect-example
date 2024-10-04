48c0f9c513f528122add9f08807473b8f7a97155
Dale Gordon <dgordon@plusonex.com>
2024-10-04 11:37:09 +0200

    8

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const thisIsAVariable = "This is commit 7";
+  const thisIsAVariable = "This is commit 8";
 
   return (
     <>
